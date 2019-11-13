450dfbf2353140bd0242e5cd7a77bac2a674df3f
Wiki Updater <admin@catbox.wtf>
2019-11-13 20:37:07 +0100

    Update utils.java

--- src/main/java/disc/utils.java
+++ src/main/java/disc/utils.java
@@ -1,7 +1,8 @@
 package disc;
 
 public class utils {
-    public static int nukeDistance = 10;
+    static int nukeDistance = 15;
+    public static Boolean antiNukeEnabled = true;;
 
     static double DistanceBetween(double x1, double y1, double x2, double y2) {
         return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));

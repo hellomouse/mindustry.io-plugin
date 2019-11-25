2fc6d98258ae055a4ea5c91184ef35862ce4432f
Wiki Updater <admin@catbox.wtf>
2019-11-25 18:53:45 +0100

    stringArrayToString

--- src/main/java/io/mindustry/plugin/Utils.java
+++ src/main/java/io/mindustry/plugin/Utils.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Utils {
     static int nukeDistance = 25;
+    static int messageBufferSize = 5; // number of messages sent at once to discord
     public static Boolean antiNukeEnabled = true;;
 
     static double DistanceBetween(double x1, double y1, double x2, double y2) {
@@ -20,5 +21,13 @@ public class Utils {
             result.append(string).append("\n");
         }
         return result + "```";
+    }
+
+    public static String stringArrayToString(List<String> array) {
+        StringBuilder result= new StringBuilder();
+        for(String string : array){
+            result.append(string);
+        }
+        return result.toString();
     }
 }

959544ce2c2b026a9a81e71de8a24ed5915fc4ab
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-02-06 15:35:58 +0100

    Split PlayerData into TempPlayerData

--- src/main/java/mindustry/plugin/PlayerData.java
+++ src/main/java/mindustry/plugin/PlayerData.java
@@ -3,26 +3,16 @@ package mindustry.plugin;
 import java.io.Serializable;
 
 public class PlayerData implements Serializable {
-    public Integer hue;
-    public String realName = "";
     public Integer rank = 0;
     public Integer playTime = 0;
     public Integer buildingsBuilt = 0;
     public Integer enemiesKilled = 0;
-    public Integer draugPets = 0;
     public Integer gamesPlayed = 0;
 
 
-    public PlayerData(Integer hue, String realName, Integer rank, Integer playTime){
-        this.hue = hue;
-        this.realName = realName;
+    public PlayerData(Integer rank, Integer playTime){
         this.rank = rank;
         this.playTime = playTime;
-    }
-
-    public PlayerData(int i, String name) {
-        this.hue = i;
-        this.realName = name;
     }
 
     public PlayerData(int rank){
@@ -35,15 +25,10 @@ public class PlayerData implements Serializable {
 
     public int getBuildings() { return buildingsBuilt; }
 
-    public int getDraugPets() { return draugPets; }
 
     public int getGames() { return gamesPlayed; }
 
     public void setRank(int rank) { this.rank = rank; }
-
-    public void setHue(int h) {
-        hue = h;
-    }
 
     public void incrementPlaytime(int i) {
         this.playTime = playTime + i;
@@ -51,14 +36,6 @@ public class PlayerData implements Serializable {
 
     public void incrementBuilding(int i) {
         this.buildingsBuilt = buildingsBuilt + i;
-    }
-
-    public void incrementDraug(int i) {
-        this.draugPets = draugPets + i;
-    }
-
-    public void resetDraug() {
-        this.draugPets = 0;
     }
 
     public void incrementGames() {
---
+++ src/main/java/mindustry/plugin/TempPlayerData.java
@@ -0,0 +1,11 @@
+package mindustry.plugin;
+import java.io.Serializable;
+
+public class TempPlayerData implements Serializable {
+    public Integer hue;
+    public String realName = "";
+    public TempPlayerData(Integer hue, String name){
+        this.hue = hue;
+        this.realName = name;
+    }
+}
